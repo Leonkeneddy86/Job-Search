40911d3a705e00117cd93a552bc4e835b36b91ff
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-25 15:29:46 +0100

    refactor: database

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -18,6 +18,8 @@ class JobFactory extends Factory
     {
         return [
             //
+            'entry' => $this->faker->realText($maxNbChars = 200),
+            'emotion' => $this->faker->imageUrl(),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Job;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,9 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::factory(1)->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Job::factory(1)->create();
+
     }
 }
