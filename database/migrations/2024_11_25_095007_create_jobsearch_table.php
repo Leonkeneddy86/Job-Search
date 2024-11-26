3771b92f3972d78d8b82a957860a6427ce0e2361
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-26 09:14:59 +0100

    refactor:database

--- database/migrations/2024_11_25_095007_create_jobsearch_table.php
+++ database/migrations/2024_11_25_095007_create_jobsearch_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('jobsearch', function (Blueprint $table) {
+        Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->text("entry");
-            $table->string("emotion");
+            $table->string('offers');
+            $table->string('updates');
             $table->timestamps();
         });
     }
