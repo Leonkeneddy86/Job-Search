266ff659451f560c7e12f39234f736602d0923df
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-26 09:22:20 +0100

    merge origin/Models/Database

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -18,8 +18,8 @@ class JobFactory extends Factory
     {
         return [
             //
-            'entry' => $this->faker->realText($maxNbChars = 200),
-            'emotion' => $this->faker->imageUrl(),
+            'offers' => $this->faker->realText($maxNbChars = 200),
+            'updates' => $this->faker->imageUrl(),
         ];
     }
 }
