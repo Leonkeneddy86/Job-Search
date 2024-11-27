7e73ab839f25566e99dc78beea84491e61bd9b25
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-27 15:44:14 +0100

    Style: blade && text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,18 +9,59 @@
         <th scope="col">Oferta</th>
         <th scope="col">Empresa</th>
         <th scope="col">Descripción</th>
-        <th scope="col">Contactos</th>
+        
       </tr>
     </thead>
     <tbody>
-      @foreach ($offers as $offer)
       <tr>
         <th scope="row">1</th>
-        <td>M</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr> 
-      @endforeach
+        <td>27/11/2024</td>
+        <td>Programador Full Stack</td>
+        <td>Factoria F5</td>
+        <td>Crear bases de datos en MySQL, con su patron de diseño (Modelo,Vista,Controlador) y tener la web en blade</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>28/12/2024</td>
+        <td>Gestion de sistemas</td>
+        <td>Amazon Web Services (AWS)</td>
+        <td>Gestionar nuestros sistemas, realizar mantenimiento y actualizaciones</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>29/13/2024</td>
+        <td>Analisis de datos</td>
+        <td>IBM</td>
+        <td>inspeccionar, limpiar y modelar datos con el objetivo de descubrir información útil, llegar a conclusiones y apoyar la toma de decisiones. </td>
+      </tr>
+      <tr>
+        <th scope="row">4</th>
+        <td>30/13/2024</td>
+        <td>Analista de Ciberseguridad</td>
+        <td>Trustwave</td>
+        <td>proteger los sistemas informáticos, redes y datos de la organización contra amenazas y ataques cibernéticos.</td>
+      </tr>
+      <tr>
+        <th scope="row">5</th>
+        <td>28/12/2024</td>
+        <td>Gestion de sistemas</td>
+        <td>Amazon Web Services (AWS)</td>
+        <td>Gestionar nuestros sistemas, realizar mantenimiento y actualizaciones</td>
+      </tr>
+      <tr>
+        <th scope="row">6</th>
+        <td>28/12/2024</td>
+        <td>Gestion de sistemas</td>
+        <td>Amazon Web Services (AWS)</td>
+        <td>Gestionar nuestros sistemas, realizar mantenimiento y actualizaciones</td>
+      </tr>
+      <tr>
+        <th scope="row">7</th>
+        <td>28/12/2024</td>
+        <td>Gestion de sistemas</td>
+        <td>Amazon Web Services (AWS)</td>
+        <td>Gestionar nuestros sistemas, realizar mantenimiento y actualizaciones</td>
+      </tr>
     </tbody>
   </table> 
 @endsection
