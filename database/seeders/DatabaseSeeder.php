63542a78c43ce450d450a3357aab60d13537d5a7
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-11-29 13:30:32 +0100

    feat:apiOffers finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+         Offer::factory(10)->create();
      // User::factory(5)->create([
      //'name' => 'Test User',
      
