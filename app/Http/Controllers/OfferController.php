b2327f088ad14c4c750c33541cc2e702d044e0eb
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-01 21:11:59 +0100

    feat:view offercontroller

---
+++ app/Http/Controllers/OfferController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class OfferControllers extends Controller
+{
+    public function index(Request $request)
+    {
+        if ($request->action === 'delete')
+        {
+            $this->destroy($request->id);
+            return Redirect::to(route('index'));
+        }
+        if ($request->action === 'pause')
+        {
+            $this->edit($request->id, 0);
+            return Redirect::to(route('index'));
+        }
+        if ($request->action === 'resume')
+        {
+            $this->edit($request->id, 1);
+            return Redirect::to(route('index'));
+        }
+        $offers = Offer::all();
+        return view('index', compact('offers'));
+
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    
+        //
+    
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    /*public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $offer = Offer::find($id);
+
+        if ($offer)
+        return view ('show', compact('offer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id, bool $status)
+    {
+        Offer::find($id)->update(['status' => $status]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    /*public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $offer = Offer::find($id);
+        if($offer)
+        {
+            $offer->delete();
+        }
+    }
+}
