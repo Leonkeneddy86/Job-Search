fb421f13826bd1439d204767fe10a91646db0a92
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-01 21:13:50 +0100

    fix:create function

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -39,9 +39,9 @@ class OfferControllers extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /*public function create()
     {
-        
+        //
     }
 
     /**
