e7ad45fab1b199a0d8bb236803a07f8f3f1a32d8
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-01 19:12:54 +0100

    fix:factories info

--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -17,7 +17,10 @@ class OfferFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'offer' => $this ->faker->realText($maxNbChars = 200),
+            'description' => $this ->faker->realText($maxNbChars = 200),
+            'company' => $this->faker->company(),
+            'status' => $this->faker->boolean(),
             
         ];
     }
