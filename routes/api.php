395d4adadb84cb1f0564c9d415fda6a9ed692d55
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-02 15:54:24 +0100

    fix title apindex

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\OfferController;
 
-Route::get('/offers', [OfferController::class, 'index'])->name('apiindex');
+Route::get('/offers', [OfferController::class, 'index'])->name('apindex');
 Route::post('/offers', [OfferController::class, 'store'])->name('apistore');
 Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apishow');
 Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiupdate');
