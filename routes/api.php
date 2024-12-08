10ef7d7ab4350705860b519d9359a436847dd9e2
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2024-12-08 14:41:21 +0100

    feat:feedbackRoutes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\OfferController;
+use App\Http\Controllers\Api\FeedbackController;
 
 Route::get('/offers', [OfferController::class, 'index'])->name('apindex');
 Route::post('/offers', [OfferController::class, 'store'])->name('apistore');
@@ -10,6 +11,11 @@ Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apishow');
 Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiupdate');
 Route::delete('/offers/{id}', [OfferController::class, 'destroy'])->name('apidestroy');
 
+Route::get('/offers/{offerId}/news', [FeedbackController::class, 'index'])->name('apiindexNews');
+Route::post('/offers/{offerId}/news', [FeedbackController::class, 'store'])->name('apistoreNews');
+Route::get('/offers/{offerId}/news/{newsID}', [FeedbackController::class, 'show'])->name('apishowNews');
+Route::put('/offers/{offerId}/news/{newsID}', [FeedbackController::class, 'update'])->name('apiupdateNews');
+Route::delete('/offers/{offerId}/news/{newsId}', [FeedbackController::class, 'destroy'])->name('apidestroyNews');
 
 
 
